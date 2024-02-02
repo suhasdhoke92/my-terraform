4479a30a3c4b528a62fcbbe5321e4c610d581f2a
suhasdhoke92 <157954208+suhasdhoke92@users.noreply.github.com>
2024-02-03 00:53:52 +0530

    Create variable.tf

---
+++ Day-2/testing/variable.tf
@@ -0,0 +1,9 @@
+variable "ec2_type" {
+    description = "to enter the type of machin"
+    // default = "t2.micro"
+}
+
+variable "ec2_ami" {
+   description = "ami details"
+   default = "ami-0d63de463e6604d0a"
+}
